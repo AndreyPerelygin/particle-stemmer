b29952d8a730d7f8806da0bf38afe04becca2f14
Andrey <andrey@zencore.org>
2017-01-20 17:07:36 +0300

    word_list

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.2'
 
 setup(name='partstem',
       version=version,
-      description="0.1",
+      description="0.2",
       long_description="""\
       Fork NLTK Snowball stemmer""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
